cebf046436c2c8786bcf622cd28907bbbac1d5ab
Xurshidbek Shodiyev <85980781+KhurshidShod@users.noreply.github.com>
2023-09-22 23:57:14 +0500

    asdasdagjhklgvhbjasd

--- src/components/order/index.jsx
+++ src/components/order/index.jsx
@@ -11,7 +11,12 @@ const OrderTab = ({ orderOpen, closeOrder, cart }) => {
       return `${price.toString().slice(0, 3)},${price.toString().slice(3)}`
     } else if(price.toString().length === 7){
       return `${price.toString().slice(0, 1)},${price.toString().slice(1, 4)},${price.toString().slice(4)}`
+    } else if(price.toString().length === 8){
+      return `${price.toString().slice(0, 2)},${price.toString().slice(2, 5)},${price.toString().slice(5)}`
+    } else if(price.toString().length === 9){
+      return `${price.toString().slice(0, 3)},${price.toString().slice(3, 6)},${price.toString().slice(6)}`
     }
+    
   }
   return (
     <div className={`${styles.order} ${orderOpen ? styles.open : null}`}>
@@ -26,13 +31,13 @@ const OrderTab = ({ orderOpen, closeOrder, cart }) => {
         <div className={styles.order__wrapper_orders}>
           {cart.map(prod => <div key={prod.id} className={styles.order__wrapper_orders_order}>
             <div>
-              <img src="https://telegra.ph//file/8cc0d9a58c68c695b2076.jpg" alt="" />
+              <img src={prod.photos.split('|')[0].replace('http', 'https')} alt="" />
               <div>
                 <h5>{prod.name} <span>{prod.quantity}x</span></h5>
                 <p>{prod.about}</p>
               </div>
             </div>
-            <h4>{checkPrice(prod.price)}</h4>
+            <h4>{checkPrice(prod.price * prod.quantity)}</h4>
           </div>)}
           <hr />
         </div>
