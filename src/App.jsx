a8c12ebdd4fef9f09885af71e57844c0e1c91f11
Xurshidbek Shodiyev <85980781+KhurshidShod@users.noreply.github.com>
2023-09-23 19:54:51 +0500

    asdasdagjhklgvhbjasd

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,14 @@ import { ToastContainer } from "react-toastify";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.min.css";
 
-window.Telegram.WebApp;
-
 function App() {
   const tele = window.Telegram.WebApp;
   useEffect(() => {
     tele.ready();
     tele.enableClosingConfirmation();
-  }, [tele]);
+  });
   return (
     <Fragment>
-      <ToastContainer
-        position="top-center"
-        autoClose={7500}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
       <HomePage />
     </Fragment>
   );
