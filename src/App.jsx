3ddfb83775f98b94c67de4412fe8bf6477be6769
Xurshidbek Shodiyev <85980781+KhurshidShod@users.noreply.github.com>
2023-09-24 16:20:20 +0500

    sdfsdfs

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         pauseOnHover
         theme="light"
       />
-      <p>hj{window.Telegram.WebApp.initData.user}</p>
+      <p>hj{window.Telegram.WebApp.initData.query_id}</p>
       <HomePage />
     </Fragment>
   );
