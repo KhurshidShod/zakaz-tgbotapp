b2fd7084ba11a6a0634b104e0f09879e4bf26ec5
Xurshidbek Shodiyev <85980781+KhurshidShod@users.noreply.github.com>
2023-09-28 21:06:15 +0500

    asdasd

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -13,7 +13,7 @@ import { LuBaby } from "react-icons/lu";
 import { FaMale } from "react-icons/fa";
 import { FaFemale } from "react-icons/fa";
 import { BsFillPersonFill } from "react-icons/bs";
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+// import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 const HomePage = () => {
   const [cat, setCat] = useState("young");
@@ -138,11 +138,11 @@ const HomePage = () => {
                 .filter((img) => img !== "None")
                 .map((img) => (
                   <div key={img}>
-                    <TransformWrapper>
-                      <TransformComponent>
+                    {/* <TransformWrapper>
+                      <TransformComponent> */}
                         <img src={img.replace("http", "https")} alt="" />
-                      </TransformComponent>
-                    </TransformWrapper>
+                      {/* </TransformComponent>
+                    </TransformWrapper> */}
                   </div>
                 ))}
             </Slider>
